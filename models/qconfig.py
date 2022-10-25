bc344108e98da004bbb0ead2e1b82ea3066b43c3
Alessandro Sebastianelli <asebastianelli@esr01109232.esa-ad.esa.int>
2022-10-25 14:44:11 +0200

    New metrics

--- models/qconfig.py
+++ models/qconfig.py
@@ -1,7 +1,7 @@
 qcnnv1s = {
             'loss':            'categorical_crossentropy',
             'learning_rate':   0.0002,
-            'metrics':         ['accuracy'],
+            'metrics':         ['accuracy', 'mse'],
             'dropout':         0.2,
             'batch_size':      16,
             'epochs':          5,
