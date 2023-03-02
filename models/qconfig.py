4c84a3882089506ca99a7e47e13a23f42140e5e0
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-03-02 13:57:28 +0100

    new config file

--- models/qconfig.py
+++ models/qconfig.py
@@ -5,9 +5,9 @@ qcnnv1s = {
             'dropout':         0.2,
             'batch_size':      16,
             'epochs':          100,
-            'early_stopping':  10,
-            'dense':           [256, 128, 64],                      # Vector of #neurons for each dense layer
-            'conv':            [128],                               # Vector of #filters for each convolution layer
+            'early_stopping':  30,
+            'dense':           [32],                                # Vector of #neurons for each dense layer
+            'conv':            None,                                # Vector of #filters for each convolution layer
             'kernel':          3,                                   # Kernel Size for the convolution
             'stride':          1,                                   # Strides for the convolution
             'pool_size':       2,                                   # Kernel Size for the Global Average Pooling
