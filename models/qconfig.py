c749b5c0eb9023db27aced3b17b9578232cbf215
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-04-13 10:15:23 +0200

    new configs

--- models/qconfig.py
+++ models/qconfig.py
@@ -1,15 +1,15 @@
 qcnnv1s = {
             'loss':            'categorical_crossentropy',
             'learning_rate':   0.0002,
-            'metrics':         ['accuracy', 'mse'],
+            'metrics':         ['accuracy'],
             'dropout':         0.2,
             'batch_size':      16,
             'epochs':          100,
             'early_stopping':  30,
-            'dense':           [32],                                # Vector of #neurons for each dense layer
-            'conv':            None,                                # Vector of #filters for each convolution layer
-            'kernel':          3,                                   # Kernel Size for the convolution
-            'stride':          1,                                   # Strides for the convolution
-            'pool_size':       2,                                   # Kernel Size for the Global Average Pooling
-            'pool_stride':     2,                                   # Strides for the Global Average Pooling
+            'dense':           [256, 128, 64, 32, 16],    # Vector of #neurons for each dense layer
+            'conv':            [12, 64, 128, 256],     # Vector of #filters for each convolution layer
+            'kernel':          3,           # Kernel Size for the convolution
+            'stride':          1,           # Strides for the convolution
+            'pool_size':       2,           # Kernel Size for the Global Average Pooling
+            'pool_stride':     1,           # Strides for the Global Average Pooling
         }
