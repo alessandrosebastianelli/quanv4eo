7bc401296d137bfdec95bebd2812e2507b42e288
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-04-14 12:28:11 +0200

    new cfgs

--- models/qconfig.py
+++ models/qconfig.py
@@ -6,10 +6,10 @@ qcnnv1s = {
             'batch_size':      16,
             'epochs':          100,
             'early_stopping':  30,
-            'dense':           [256, 128, 64, 32, 16],    # Vector of #neurons for each dense layer
-            'conv':            [12, 64, 128, 256],     # Vector of #filters for each convolution layer
+            'dense':           [32, 16],    # Vector of #neurons for each dense layer
+            'conv':            None,     # Vector of #filters for each convolution layer
             'kernel':          3,           # Kernel Size for the convolution
             'stride':          1,           # Strides for the convolution
             'pool_size':       2,           # Kernel Size for the Global Average Pooling
-            'pool_stride':     1,           # Strides for the Global Average Pooling
+            'pool_stride':     2,           # Strides for the Global Average Pooling
         }
